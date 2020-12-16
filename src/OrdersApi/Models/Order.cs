d1955fcaf961caca260d4bdf55d2339c082c2edd
Casper Sørensen <caspers@live.dk>
2020-12-16 10:56:45 +0100

    the api can get data from the mongo database

--- src/OrdersApi/Models/Order.cs
+++ src/OrdersApi/Models/Order.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -6,12 +7,13 @@ namespace OrdersApi.Models
 {
   public class Order
   {
-
     [BsonId]
     public ObjectId InternalId { get; set; }
     public long Id { get; set; }
-    public int OrderNo { get; set; }
-    public string Content { get; set; }
+    public string customer_name { get; set; }
+    public string customer_address { get; set; }
+    public string customer_zipcode { get; set; }
+    public int product { get; set; }
+    public int product_quantity { get; set; }
   }
-
 }
